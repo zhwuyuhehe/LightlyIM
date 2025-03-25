2be93484fcfe159d3815b4213a0edef52bcb3ffb
zhwuyuhehe <1710734972@qq.com>
2025-03-25 23:07:15 +0800

    初始化项目

---
+++ src/main/java/top/zspaces/lightlyim/LightlyImApplication.java
@@ -0,0 +1,13 @@
+package top.zspaces.lightlyim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightlyImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightlyImApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/zspaces/lightlyim/LightlyImApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zspaces.lightlyim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightlyImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
