c7e2077832a31d40a3cf8d4fc248fa0e01f039eb
zhwuyuhehe <1710734972@qq.com>
2025-04-06 22:07:03 +0800

    设置MapperScan

--- src/main/java/top/zspaces/lightlyim/LightlyImApplication.java
+++ src/main/java/top/zspaces/lightlyim/LightlyImApplication.java
@@ -1,9 +1,11 @@
 package top.zspaces.lightlyim;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@MapperScan("top.zspaces.lightlyim.mapper")
 @EnableAsync
 @SpringBootApplication
 public class LightlyImApplication {
